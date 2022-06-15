fa622b2cef17c212f826b5e7101f6fb2cff2be20
cfan310 <63741693+cfan310@users.noreply.github.com>
2022-06-15 18:46:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+/*var amount = 3;
+
+console.log(amount);
+ 
+//Python Code
+roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+class Solution:
+    def romanToInt(self, S: str) -> int:
+        ans = 0
+        for i in range(len(S)-1,-1,-1):
+            num = roman[S[i]]
+            if 4 * num < ans: ans -= num
+            else: ans += num
+        return ans
+
+// JAVASCRIPT CODE
+
+const roman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+var romanToInt = function (S) {
+  let ans = 0;
+  for (let i = S.length - 1; ~i; i--) {
+    let num = roman[S.charAt(i)];
+    if (4 * num < ans) ans -= num;
+    else ans += num;
+  }
+  return ans;
+};
+
+romanToInt(roman[0]);
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const adult > 18;
+
+const youth < 18;
+
+/*
+console output: 
+Brandon is an adult and he registered early at
+ ${time}.
+OR
+Brandon is an adult and registered late at
+${time}.
+OR
+Brandon is a youth and registered at
+${time}.
+
+const runnerName = ''; 
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+console.log(racenumber);
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const adult > 18;
+
+const youth < 18;
+
+/*
+console output: 
+Brandon is an adult and he registered early at
+ ${time}.
+OR
+Brandon is an adult and registered late at
+${time}.
+OR
+Brandon is a youth and registered at
+${time}.
+
+const runnerName = ''; 
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+console.log(racenumber);
+
+
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const adult > 18;
+
+const youth < 18;
+
+
+console output: 
+Brandon is an adult and he registered early at
+ ${time}.
+OR
+Brandon is an adult and registered late at
+${time}.
+OR
+Brandon is a youth and registered at
+${time}.
+
+
+// number assigned based on age and registration
+
+const runnerName = "";
+
+// early adult runners recieve a number >= 1,000
+// to generate a random number between two numbers (between 1,000 and 5,000)
+
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const rndInt = randomIntFromInterval(1000, 2000);
+//console.log(rndInt);
+
+let earlyAdultRaceNumber = rndInt;
+
+// all other members recieve a number below 1,000
+let othersRaceNumber = Math.floor(Math.random() * 999) + 1; // +1 so it's (0-998 inclusive, +1) (so output is 1 - 999)
+
+console.log(earlyAdultRaceNumber);
+console.log(othersRaceNumber);
+*/
+
+const width = 10;
+const height = 12;
+const area = length * width;
+
+console.log(area);
